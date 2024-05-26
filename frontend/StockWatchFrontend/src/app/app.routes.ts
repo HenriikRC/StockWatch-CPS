204abc8908559c0ee41634295445c9a7fca827d7
deehstar <moand22@student.sdu.dk>
2024-05-26 22:02:36 +0200

    stock component created

--- frontend/StockWatchFrontend/src/app/app.routes.ts
+++ frontend/StockWatchFrontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { StockComponent } from './stock/stock.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '', component: StockComponent, title: 'Stock Watch'
+}];
